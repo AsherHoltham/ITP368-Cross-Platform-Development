70f9b62b5f67e3760c4142048fab7d610c281a8a
AsherHoltham <asher.holtham.github@gmail.com>
2025-01-28 12:44:08 -0800

    Dice Lab

---
+++ Dice/dice/lib/dice.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Dice'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool firstRoll = true;
+  int dice = 0;
+
+  void roll() {
+    setState(() {
+      dice = Random().nextInt(6) + 1;
+      firstRoll = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Dice output:',
+            ),
+            Text(
+              firstRoll ? "Roll the Die" : '$dice',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            FloatingActionButton(
+              onPressed: roll,
+              tooltip: 'Roll',
+              child: const Icon(Icons.circle),
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
